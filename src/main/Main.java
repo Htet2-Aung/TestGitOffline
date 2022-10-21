9b5c0cc8afca7a947790b52ec61653125c5b6476
Htet2-Aung <116067792+Htet2-Aung@users.noreply.github.com>
2022-10-21 12:48:17 +0630

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,9 +7,6 @@ public class Main {
 		System.out.println("Hello Git Version Control.");
 		System.out.println("Hello Git Version Control Changed-1.");
 		System.out.println("Test SSH success");
-		
-		
-		
 		System.out.println("Shopping Cart Version");
 	}
 
